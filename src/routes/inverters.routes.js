be04aac8b2d73ab66a83e526a67da593dae42f20
fercho524 <mrxd9767@gmail.com>
2024-07-09 15:51:20 -0600

    Fix in inverter routes

--- src/routes/inverters.routes.js
+++ src/routes/inverters.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import * as dataloggerController from "../controllers/inverters.controllers.js";
+import * as inverterController from "../controllers/inverters.controllers.js";
 import { verifyToken } from '../middlewares/auth.middleware.js';
 import { growattLogin } from "../middlewares/growattLogin.middleware.js";
 
 const router = Router()
 
 
-router.get('/', verifyToken,growattLogin,dataloggerController.listInverters)
+router.get('/:id', verifyToken,growattLogin,inverterController.getInvertersByPlant)
 
 
 export default router;
